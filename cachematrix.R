bef3c8c8cb80d183c9d6449c9d5136f0e96acb00
douglaspampu <douglascpampu@gmail.com>
2014-11-23 11:22:55 -0200

    Rebuild the code using solve function

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,6 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-escalona_linha <- function (e, m, e1){
-	e <- e - m*e1
-
-	e
-}
-
-
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
@@ -15,35 +8,15 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function uses the solve built in function to return the inverse
+## matrix of x
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 
-	x_inv <- matrix (,NROW(x), NCOL(x))
+	## Calculate the inverse of x usin solve(x) function
+	inverse_x <- solve (x)
 
-	for (i in 1:NROW(x))
-	{
-		for (j in 1:NCOL(x))
-		{
-			if (i == j)
-			{
-				x_inv[i,j] <- 1
-			}
-			else
-			{
-				x_inv[i,j] <- 0
-			}
-		}
-	}
-
-	for (i in 2:NROW(x))
-	{
-		l <- lapply (x_inv[i,], escalona_linha, x[i,i-1]/x[i-1,i-1], x[i-1,i-1])
-	
-		for (j in 1:NCOL(x))
-		{
-			x_inv[i,j] <- l[j]
-		}
-	}
+	##retuns the inverse of x
+	inverse_x
 }
