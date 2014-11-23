107bcccd38289003b651d0a0f460762906af0749
douglaspampu <douglascpampu@gmail.com>
2014-11-23 11:06:02 -0200

    Matrix inversion done

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,13 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+escalona_linha <- function (e, m, e1){
+	e <- e - m*e1
+
+	e
+}
+
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
@@ -12,4 +19,31 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+	x_inv <- matrix (,NROW(x), NCOL(x))
+
+	for (i in 1:NROW(x))
+	{
+		for (j in 1:NCOL(x))
+		{
+			if (i == j)
+			{
+				x_inv[i,j] <- 1
+			}
+			else
+			{
+				x_inv[i,j] <- 0
+			}
+		}
+	}
+
+	for (i in 2:NROW(x))
+	{
+		l <- lapply (x_inv[i,], escalona_linha, x[i,i-1]/x[i-1,i-1], x[i-1,i-1])
+	
+		for (j in 1:NCOL(x))
+		{
+			x_inv[i,j] <- l[j]
+		}
+	}
 }
